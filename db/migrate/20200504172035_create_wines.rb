e2ef233020ef7a9705b36e9e58f0e84383c8b92e
ashleymader <ashley.mader12@gmail.com>
2020-05-04 15:55:49 -0400

    changed column name to w_type

--- db/migrate/20200504172035_create_wines.rb
+++ db/migrate/20200504172035_create_wines.rb
@@ -1,7 +1,7 @@
 class CreateWines < ActiveRecord::Migration
   def change
     create_table :wines do |t|
-      t.string :type
+      t.string :w_type
       t.integer :quantity
       t.string :vintage
       t.text :varietal
