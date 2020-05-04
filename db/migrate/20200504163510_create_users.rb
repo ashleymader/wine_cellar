cd49af75e654f76fa785a4cfb19c15e83ed0a2bf
ashleymader <ashley.mader12@gmail.com>
2020-05-04 15:57:35 -0400

    added colums to user table

--- db/migrate/20200504163510_create_users.rb
+++ db/migrate/20200504163510_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :image_url
+      t.string :email
       t.string :password_digest
+      t.string :image_url
       t.timestamps null: false
     end
   end
