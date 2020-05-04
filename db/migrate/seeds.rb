4611e73215075cb490ad17b215b31d71875d7456
ashleymader <ashley.mader12@gmail.com>
2020-05-04 14:38:50 -0400

    added seed users

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,6 @@
+#this is where I will write the seed data for my program 
+
+ashley = User.create(email: "ashley@ashley.com", password: "pw", image_url: "https://images.app.goo.gl/zrPRXKw3nQEiyQeW7")
+
+nick = User.create(email: "nick@nick.com", password: "pw", image_url: "https://images.app.goo.gl/SDiSi6iKe5yoiU4q7") 
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200504172035) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "wines", force: :cascade do |t|
+    t.string   "type"
+    t.integer  "quantity"
+    t.string   "vintage"
+    t.text     "varietal"
+    t.text     "producer"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
