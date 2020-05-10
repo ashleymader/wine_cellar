08e928300fcde30853409d0581710b09fa2a8b6d
ashleymader <ashley.mader12@gmail.com>
2020-05-10 14:30:01 -0400

    login, authenticate, sessions all enabled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    #enable sessions and allow it to be inherited to all other controller (so we can track across pages)
+    enable :sessions
+    #set a secret session for security. Always name it something not obvious
+    set :session_secret, "i_feel_so_smart_right_now"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,24 @@ class UsersController <ApplicationController
 
     #render my login form for a user 
     get '/login' do 
-        erb :"users/login"
+        erb :'users/login'
     end
 
+    #recieving params from users/login.erb
+    post '/login' do 
+        #find user 
+        user = User.find_by(email: params[:email])
+        #authenticate user 
+        # we add if user && because if email isn't found (mistype or whatever, it won't return nil on user. it will just redirect to login page again)
+        if user && user.authenticate(params[:password])
+            binding.pry
+        #login user 
+        session[:user_id] = user.id
+        #redirect to the user/show page
+        else  
+        #redirect to login or register 
+            redirect '/login'
+        end
+    end
     
 end
