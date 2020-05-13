4771cf821a4890eb6f909f3ed2b4389b157fbdeb
ashleymader <ashley.mader12@gmail.com>
2020-05-13 14:08:04 -0400

    added wine controller not sure of use yet

---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,17 @@
+# class WinesController <ApplicationController 
+
+
+#     get '/wines' do 
+#         @wines = Wine.all 
+#         erb :"/wines/index"
+#     end
+
+#     get '/wines/:id' do 
+#         @wines = Wine.find_by_id(params[:id])
+#         erb :"/wines/show"
+#     end
+
+#     post 
+
+
+# end
--- config.ru
+++ config.ru
@@ -8,5 +8,6 @@ end
 #here is where I will mount other controllers with the keyword 'use'
 use UsersController
 use UserWinesController
+#use WinesController
 #need rack::methodoverride to send patch and delete requests
 run ApplicationController
