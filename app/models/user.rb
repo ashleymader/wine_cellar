d95740eb40e884956bc83987ebe294d38b20a64a
ashleymader <ashley.mader12@gmail.com>
2020-06-29 14:19:34 -0400

    dependent: destroy - to ensure deletion of record

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
     has_secure_password
 
     has_many :wines, through: :user_wines 
-    has_many :user_wines
+    has_many :user_wines, dependent: :destroy
 
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,7 +1,7 @@
 class Wine < ActiveRecord::Base
 
     has_many :users, through: :user_wines 
-    has_many :user_wines
+    has_many :user_wines, dependent: :destroy
 
     # validates :w_type, :vintage, :varietal, :producer, presence: true
 end
